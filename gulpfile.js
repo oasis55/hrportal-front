4179d0989e3c1c4958cc30ce0ec97c9cef25a07a
Vladimir <geneart,ru@gmail.com>
2016-06-01 19:13:59 +0500

    init

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp         = require('gulp'),
+    jshint       = require('gulp-jshint'),
+    concat       = require('gulp-concat'),
+    rename       = require('gulp-rename'),
+    uglify       = require('gulp-uglify'),
+    minifyCss    = require('gulp-minify-css'),
+    sass         = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sass', function () {
+
+    return gulp
+        .src('scss/index.scss')
+        .pipe(sass()).on('error', sass.logError)
+        .pipe(autoprefixer())
+        .pipe(minifyCss())
+        .pipe(rename('index.min.css'))
+        .pipe(gulp.dest('css'));
+
+});
+
+gulp.task('default', ['sass'], function () {
+
+    gulp.watch(['scss/*.scss', 'scss/**/*.scss'], ['sass']);
+
+});
